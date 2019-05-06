6ef77dd8bce0a28bedf9af5c1c466fb4a4540b08
zzy <925383908@qq.com>
2019-05-06 23:54:59 +0800

    test

---
+++ www/app.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Tests for python37 web app
+Created on Mon May  6 22:55:48 2019
+
+@author: 92538
+"""
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body='<h1>Awesome</h1>'.encode('utf-8'), content_type='text/html')
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()

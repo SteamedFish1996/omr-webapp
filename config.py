aad0b7f058c1ffb25e9f6f3aeaea8e1e4b710d95
dtj <347680079@qq.com>
2019-05-15 10:13:19 +0800

    修改配置文件

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ __author__ = 'zzy'
 # MySQL 配置文件
 SECRET_KEY = 'hard to guess'    # 一个字符串，密码。也可以是其他如加密过的
 #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@127.0.0.1:3306/omr'    # 在此登录的是root用户，密码password，MySQL端口是3306。数据库名omr
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:zzy19961220@114.115.164.99/omr
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:zzy19961220@114.115.164.99/omr'
 #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@59.77.7.58:3306/omr'   
 # 设置下方这行code后，在每次请求结束后会自动提交数据库中的变动
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
@@ -20,3 +20,6 @@ SQLALCHEMY_TRACK_MODIFICATIONS = True
 UPLOAD_FOLDER = './data/uploads'
 #ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 ALLOWED_EXTENSIONS = set(['pdf', 'png'])
+
+PYTHON27_DIR = ''
+OMR_DIR = ''

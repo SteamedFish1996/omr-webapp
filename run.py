7b9d8e70942e5c03206750e02593202f2b0f7b44
zzy <925383908@qq.com>
2019-05-17 00:19:10 +0800

    启动时加入提示

--- run.py
+++ run.py
@@ -8,6 +8,19 @@ __author__ = 'zzy'
 from flask_cors import CORS
 from www import app
 
+
+import os
+from omr.shell import Shell
+basepath = os.path.dirname(__file__)  # 当前文件所在路径
+vue_dir = basepath+'/www/templates/vueweb'
+cmd1 = "cd "+vue_dir
+cmd2 = 'npm run build'
+cmd3 = 'npm run dev'
+cmd = cmd1 + ' && ' + cmd2 + ' && ' + cmd3
+print('run this command in shell to start vue quickly:')
+print(cmd)
+print()
+
 CORS(app, supports_credentials=True)
 app.debug = True    #交互式调试器 WARNING: Do not use the development server in a production environment.
 app.run(host='0.0.0.0')

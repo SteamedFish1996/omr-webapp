598ac4229b11a55cb8c4fbbdea113a3409ada65c
zzy <925383908@qq.com>
2019-07-07 22:27:24 +0800

    保存-处理结果

--- www/test.py
+++ www/test.py
@@ -12,39 +12,41 @@ def omr(image_name):
     #
     omr_dir = '/home/ps/zzy/music/moonlight/bazel-bin/moonlight/omr'
     output_dir = basepath + '/result'
-    input_dir = basepath + '/images/'
+    input_dir = basepath +  '/static/images/'
     cmd1 = "activate py27"
     if os_tool.system() == 'Linux':
         cmd1 = "source activate py27"
     cmd2 = omr_dir + ' ' + '--output_type=MusicXML'+ ' ' + '--output=' + output_dir + ' ' + input_dir + image_name
-
-
-    cmd = cmd1 + '&&' + cmd2 + '&&' +  'python -V'
+    cmd = cmd1 + ' && ' + cmd2 + ' && ' +  'python -V'
     print(cmd)
-    """
     shell = Shell()
-    returncode, sout, serr, pid = shell.run_cmd(cmd)
-    results = shell.cmd_result_list(sout)
-    for l in results:
-        print(l)
-"""
+    shell.run_cmd(cmd)
+    # moonlight END
+    # cnn-lstm START
+    model_dir = basepath + '/../omr/cnn_rnn'
+    images_dir = basepath + '/static/images/'
+    cmd1 = "activate tensorflow "
+    if os_tool.system() == 'Linux':
+        cmd1 = "source activate tensorflow"
+    cmd2 = "cd " +model_dir
+    cmd3 = " python standalone_inference_over_image.py \
+        --detection_inference_graph muscima-pp.pb \
+        --input_image " + image_name + "--detection_label_map category_mapping.txt \
+        --output_image result.jpg \
+        --output_result output_transcript.txt"
+    cmd = cmd1 + ' && ' + cmd2 + ' && ' +  'python -V'
+    shell = Shell()
+    shell.run_cmd(cmd)
+
+
 if __name__ == '__main__':
     image_name = 'IMSLP00747-000.png'
     omr(image_name)
 
 
 """
-    # moonlight END
 
-# cnn-lstm START
-python standalone_inference_over_image.py \
-    --detection_inference_graph 2018-05-15_faster-rcnn_inception-resnet-v2_2000-proposals_full-page-detection_muscima-pp.pb \
-    --input_image w-21_p008.png \
-    --detection_label_map category_mapping.txt \
-    --output_image annotated_image.jpg \
-    --output_result output_transcript.txt
 
-model_dir = basepath + '/../omr/cnn_rnn'
 pb_dir = basepath + '/../omr/cnn_rnn'
 input_dir = basepath + '/images/'
 output_dir = basepath + '/result'
